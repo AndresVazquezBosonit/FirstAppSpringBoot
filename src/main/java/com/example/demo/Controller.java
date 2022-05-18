733bf3811664cb7da12f60b4a807ff0bdd78f701
Andrés Vázquez <andres.vazquez@bosonit.com>
2022-05-18 18:29:57 +0200

    path variables and optional

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,18 +1,43 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController()
 public class Controller {
-    @GetMapping("/home")
-    public String getHome()
-    {
-        return "Home";
+  /*
+  @GetMapping("/user/{username}")
+  @ResponseBody
+  public String getUser(@PathVariable("username") String userName) {
+    return "The user is:  " + userName;
+  }
+  */
+
+  // multiple Path variables in a single Requests
+  @GetMapping("/user/{username}/{id}")
+  @ResponseBody
+  public String getUserAndId(
+      @PathVariable("username") String userName, @PathVariable("id") String id) {
+    return "The username is:  " + userName + " and ID is: " + id;
+  }
+
+  // Using java.util.Optional
+  @GetMapping(value = {"/user/", "/user/{username}"})
+  @ResponseBody
+  public String getUserOptional(@PathVariable("username") Optional<String> userName) {
+    if (userName.isPresent()) {
+      return "The username is:  " + userName.get();
+    } else {
+      return "username invalid";
     }
-    @GetMapping("/about")
-    public String getAbout()
-    {
-        return "About";
-    }
+  }
+
+  @GetMapping("/about")
+  public String getAbout() {
+    return "About";
+  }
 }
